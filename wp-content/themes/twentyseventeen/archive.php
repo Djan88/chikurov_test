95d38f0c8f21717830dd920968b605052624f61b
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-11-23 15:55:05 +0300

    posts-4

--- wp-content/themes/twentyseventeen/archive.php
+++ wp-content/themes/twentyseventeen/archive.php
@@ -26,32 +26,36 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php
-		if ( have_posts() ) : ?>
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+		<?php if(is_category(2)) { ?>
+            <div>1</div>
+        <?php } else { ?>
+            <?php
+            if ( have_posts() ) : ?>
+                <?php
+                /* Start the Loop */
+                while ( have_posts() ) : the_post();
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/post/content', get_post_format() );
+                    /*
+                     * Include the Post-Format-specific template for the content.
+                     * If you want to override this in a child theme, then include a file
+                     * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                     */
+                    get_template_part( 'template-parts/post/content', get_post_format() );
 
-			endwhile;
+                endwhile;
 
-			the_posts_pagination( array(
-				'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
-				'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
-			) );
+                the_posts_pagination( array(
+                    'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
+                    'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
+                    'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
+                ) );
 
-		else :
+            else :
 
-			get_template_part( 'template-parts/post/content', 'none' );
+                get_template_part( 'template-parts/post/content', 'none' );
 
-		endif; ?>
+            endif; ?>
+        <?php }  ?>
             <?php if (  $wp_query->max_num_pages > 1 ) : ?>
                 <script>
                     var ajaxurl = '<?php echo site_url() ?>/wp-admin/admin-ajax.php';
