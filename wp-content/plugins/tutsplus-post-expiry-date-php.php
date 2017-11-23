b4a0bea2b3423d9bb7440470c64dc45db95b335b
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-11-23 11:39:23 +0300

    posts-11

--- wp-content/plugins/tutsplus-post-expiry-date-php.php
+++ wp-content/plugins/tutsplus-post-expiry-date-php.php
@@ -71,4 +71,27 @@ function tutsplus_save_expiry_date_meta( $post_id ) {
 }
 add_action( 'save_post', 'tutsplus_save_expiry_date_meta' );
 
+function tutsplus_filter_expired_posts( $query ) {
+
+    // doesn't affect admin screens
+    if ( is_admin() )
+        return;
+    // check for main query
+    if ( $query->is_main_query() ) {
+
+        //filter out expired posts
+        $today = date('d-m-Y');
+        $metaquery = array(
+            array(
+                'key' => 'expires',
+                'value' => $today,
+                'type' => 'DATE',
+                'compare' => '<'
+            )
+        );
+        $query->set( 'meta_query', $metaquery );
+    }
+}
+add_action( 'pre_get_posts', 'tutsplus_filter_expired_posts' );
+
 ?>
