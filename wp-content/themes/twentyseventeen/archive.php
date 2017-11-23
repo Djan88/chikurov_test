a7f218dd6f50b4aecb283a929f234f189447cdae
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-11-23 16:43:29 +0300

    posts-13

--- wp-content/themes/twentyseventeen/archive.php
+++ wp-content/themes/twentyseventeen/archive.php
@@ -29,20 +29,20 @@ get_header(); ?>
 		<?php if(is_category(2)) { ?>
             <?php
             // The Query
-            $query = new WP_Query(
+            $querySeminar = new WP_Query(
                 array( 'category_name' => 'seminary', 'posts_per_page' => '2' )
             );
-            while ($query->have_posts()) : $query->the_post();
+            while ($querySeminar->have_posts()) : $querySeminar->the_post();
                 get_template_part( 'template-parts/post/seminar', get_post_format() );
             endwhile;
             wp_reset_postdata();
             ?>
-            <?php if (  $wp_query->max_num_pages > 1 ) : ?>
+            <?php if (  $querySeminar->max_num_pages > 1 ) : ?>
                 <script>
                     var ajaxurl = '<?php echo site_url() ?>/wp-admin/admin-ajax.php';
-                    var true_posts = '<?php echo serialize($wp_query->query_vars); ?>';
+                    var true_posts = '<?php echo serialize($querySeminar->query_vars); ?>';
                     var current_page = <?php echo (get_query_var('paged')) ? get_query_var('paged') : 1; ?>;
-                    var max_pages = '<?php echo $wp_query->max_num_pages; ?>';
+                    var max_pages = '<?php echo $querySeminar->max_num_pages; ?>';
                 </script>
                 <div id="true_loadmore">Загрузить ещё</div>
             <?php endif; ?>
