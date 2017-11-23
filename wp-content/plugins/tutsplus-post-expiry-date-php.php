511059448cfe81d68a796c2ae608387bed5f7dde
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-11-23 10:53:44 +0300

    posts-5

---
+++ wp-content/plugins/tutsplus-post-expiry-date-php.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Add an Expiry Date to Posts
+Plugin URI: http://.tutsplus.com/tutorials/add-an-expiry-date-to-wordpress-posts--cms-22665
+Description: Adds an expiry date to posts, using a the jQuery UI datepicker
+Author: Rachel McCollin
+Version: 1.0
+ */
+
+?>
