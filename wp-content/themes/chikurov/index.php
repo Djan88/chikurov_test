6fd1c56ae95a1ab72c70453e90ad0f2949220599
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-12-04 13:23:49 +0300

    chikurov

---
+++ wp-content/themes/chikurov/index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+  <title><?php the_title(); ?></title>
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/font-awesome.min.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
+  <script src="<?php bloginfo('template_url'); ?>/js/modernizr.js"></script>
+  <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+  <?php wp_head(); ?>
+</head>
+<body>
+  
+  <!-- ====================================================
+  header section -->
+  <header class="top-header">
+    <div class="container">
+      <div class="row">
+        <div class="media_area col-md-2 col-xs-4">
+          <a target="_blank" href="http://vk.com/id139677998" class="media_area_item media_area_item-vk"><i class="fa fa-vk" aria-hidden="true"></i></a>
+          <a target="_blank" href="https://www.facebook.com/profile.php?id=100012253260685&pnref" class="media_area_item media_area_item-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+          <a target="_blank" href="https://www.youtube.com/user/ThePractik01/" class="media_area_item media_area_item-youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+          <a target="_blank" href="https://www.instagram.com/yuchikurov/" class="media_area_item media_area_item-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+        </div>
+        <div class="col-md-9 col-xs-12 main-menu">
+          <div class="row">
+            <!--<div class="col-md-12 contact_area">
+              <div class="row">
+                 <div class="col-md-8 phone_i_email pull-right"><a href="mailto:info@chikurov.com">info@chikurov.com</a> | +7 (495) 135-25-48</div> 
+              </div>
+            </div>-->
+            <div class="col-md-12">
+              <nav class="navbar navbar-default">
+                <div class="container-fluid nav-bar">
+                  <!-- Brand and toggle get grouped for better mobile display -->
+                  <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                      <span class="sr-only">Toggle navigation</span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                    </button>
+                  </div>
+
+                  <!-- Collect the nav links, forms, and other content for toggling -->
+                  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    
+                    <ul class="nav navbar-nav navbar-left">
+                      <li><a class="menu active" href="#seminars">Семинары</a></li>
+                      <!-- <li><a class="menu" href="#about">Биологическое центрирование</a></li> -->
+                      <li><a class="menu" href="#service">Визардтерапия</a></li>
+                      <li><a class="menu" href="#team">Заказать видеокурс</a></li>
+                      <li><a class="menu" href="#kontakts">Контакты</a></li>
+                    </ul>
+                  </div><!-- /navbar-collapse -->
+                </div><!-- / .container-fluid -->
+              </nav>
+            </div>     
+          </div>
+        </div>
+        <div class="col-xs-2 col-sm-2 col-md-1 header-logo center-block">
+          <br>
+          <a href="index.html"><img src="img/bc_2.png" alt="" class="img-responsive logo"></a>
+        </div>
+      </div>
+    </div>
+  </header> 
+  <!-- end of header area -->
+<?php get_header(); ?>
+  <section class="doctor text-center" id="doctor">
+    <div class="container">
+      <div class="row">
+        <!-- <div class="col-md-12"><h2>Чикуров Юрий Валентинович</h2></div> -->
+        <div class="col-md-12">
+          <h4>
+            Доктор Ю. Чикуров, кандидат медицинских наук, доцент, специалист в области неврологии и нейрофизиологии. 
+          </h4>
+          <img class="img-responsive pull-left content_img" src="img/doc.jpg" alt="">
+          <h4>Как я организую свой частный приём и работаю с клиентами: </h4>
+          <p>  
+            Имея классическое врачебное образование (врач-невролог) и ученую степень кандидата медицинских наук я перепробовал в своей практической работе практически все методы традиционной и нетрадиционной медицины. 
+            В настоящее время я использую в работе принципы и технологии Биологического Центрирования - направления, созданного мною на базе более чем 30 летнего опыта практической и научной работы и связавшего воедино причины наших болезней и переживаний, ход мыслей и наше жизнеустройство в единое целое (подробности в моей одноимённой книге). Ко мне обращаются за помощью люди с широким кругом проблем - от болей в спине и шее до психосоматики, личностных нарушений и проблемных жизненных ситуаций. 
+            Работа с моими клиентами происходит в несколько этапов:
+          </p>
+          <p>
+            <b>1.</b> Ознакомившись с жалобами и пожеланиями клиента я решаю, смогу я помочь этому человеку или нет - для этого у меня есть четкие критерии, наработанные за десятилетия практической деятельности. Только после этого я назначаю дату и время приёма. Я стараюсь чтобы люди не ждали в очереди более двух недель. Общение с клиентами я начинаю с переписке через мессенджеры или емейл <a href="mailto:yuchikurov@gmail.com">yuchikurov@gmail.com</a> Если кто обращается ко мне впервые, то лучше сначала написать мне подробно о своих проблемах, а дальше, приняв решение о проведении терапии, я направляю клиента к своему администратору на тел. <b>+7 (495) 135-25-48</b> для уточнения даты, времени визита, как добраться и прочее. 
+          </p>
+          <p>
+             <b>2.</b> Сам приём может быть очный и тогда я дополнительно работаю руками с телом человека - нахожу и убираю напряжения, реконструирую последствия травмы, восстанавливаю правильные рефлексы жизнедеятельности. Есть проблемы которые лучше решать именно таким образом. Приём клиента также может быть дистантным (по актуализированному фото и описанию проблем) - в ряде ситуаций это даже эффективнее чем очный приём и тогда я работаю специально разработанными для таких случаев технологиями, имеющими обобщенное название "Визардтерапия" (это могут быть различные типы Wizardmachine или что иное). 
+          </p>
+          <p>
+            <b>3.</b> Продолжительность моей работы с клиентом составляет до одного часа, я стараюсь не превышать это время, так как особой пользы это не принесёт. 
+          </p>
+          <p>
+            <b>4.</b> Эффективность первого приема клиента обычно достигает 50-70% и разворачивается в течение первой недели после работы с ним. Бывают ситуации, когда мы с клиентом решаем ограничиться одним сеансом и этого оказывается более чем достаточно.
+          </p>
+          <p>
+            <b>5.</b> Если первый сеанс пользы не принёс (причины разные, к счастью редко), то я не вижу смысла дальше заниматься этим человеком, рекомендую обратиться к другому специалисту и полностью возвращаю гонорар.
+          </p> 
+          <p>
+            <b>6.</b> Чаще всего, если клиент заинтересован в позитивных изменениях своего здоровья и жизнеустройства в целом, я рекомендую посетить до 4-х сеансов, которые я провожу с промежутком от одной до нескольких недель. Практика показывает, что прохождение 4-х сеансов даёт более стойкий и продолжительный результат. Далее, при необходимости клиент может обратиться в любое время за поддержкой и помощью, но я стараюсь не превращаться в универсальную таблетку "от всего" и регулирую эти посещения так, чтобы это не было слишком часто.
+          </p>
+          <img class="img-responsive pull-left content_img" src="img/chi_img.jpg" alt="">
+          <h4>Как я обучаю на семинарах:</h4>
+          <p>
+            У меня очень большой стаж преподавания, более 25 лет. Так уж получилось, что я рано начал преподавать на ФУВе (факультет усовершенствования врачей). Мой подход к преподаванию очень прост и эффективен:<br>
+            <b>1.</b> Объяснять надо просто и доходчиво; <br>
+            <b>2.</b> Группы должны быть не очень большие - я стараюсь ограничивать до <b>12</b> человек, тогда я могу уделить внимание всем и "подержать за руку" каждого; <br>
+            <b>3.</b> Сначала надо пролечить, убрать ограничения, а потом обучить - именно так ставится перцепция; <br>
+            <b>4.</b> Таких слов как "я не умею" или "я невезучая" у нас на семинаре обычно не произносят - я нахожу слова поддержки для каждого, у кого занижена самооценка.<br>
+            <b>5.</b> Снимаю видео хорошего качества и потом раздаю его участникам - потом всегда можно просмотреть и вспомнить все! И помните - не боги горшки обжигают! Есть технология, если все правильно применять, то все получится стопудово!
+          </p>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section><!-- end of doctor section -->
+  <section class="seminars text-center" id="seminars">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12"><h2>Расписание семинаров</h2></div>
+        <div class="col-md-12 seminar_wrap">
+          <div class="row">
+            <div class="seminar_item col-md-12">
+              <div class="row">
+                <div class="seminar_time col-md-12">
+                  <span class="fa fa-calendar"></span> 29.11.2017 — 1.12.2017
+                  <span class="fa fa-map-marker"></span><b>Город:</b>  Москва<br>
+                </div>
+                <div class="seminar_title col-md-12">
+                  <a href="seminar.html">Базовый семинар по Биологическому центрированию</a>
+                </div>
+              </div>
+            </div>
+            <div class="seminar_item col-md-12">
+              <div class="row">
+                <div class="seminar_time col-md-12">
+                  <span class="fa fa-calendar"></span> 19.12.2017 — 19.12.2017
+                  <span class="fa fa-map-marker"></span><b>Город:</b>  Москва<br>
+                </div>
+                <div class="seminar_title col-md-12">
+                  <a href="seminar.html">Постановка перцепции</a>
+                </div>
+              </div>
+            </div>
+            <div class="seminar_item col-md-12">
+              <div class="row">
+                <div class="seminar_time col-md-12">
+                  <span class="fa fa-calendar"></span> 24.12.2017 — 24.12.2017
+                  <span class="fa fa-map-marker"></span><b>Город:</b>  Москва<br>
+                </div>
+                <div class="seminar_title col-md-12">
+                  <a href="seminar.html">Коррекция травм личной истории, круг времени</a>
+                </div>
+              </div>
+            </div>
+            <div class="seminar_item col-md-12">
+              <div class="row">
+                <div class="seminar_time col-md-12">
+                  <span class="fa fa-calendar"></span> 19.01.2018 — 21.01.2018
+                  <span class="fa fa-map-marker"></span><b>Город:</b>  Москва<br>
+                </div>
+                <div class="seminar_title col-md-12">
+                  <a href="seminar.html">Терапевтическая дефрагментация актуальных и латентных психосоматических паттернов</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-12 more_btn">
+              <div href="seminar.html" class="btn btn-primary btn-lg">Загрузить еще...</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section><!-- end of doctor section -->
+<?php get_footer(); ?>  
+  <footer class="footer clearfix">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-6 footer-para">
+          <p>&copy; Юрий Чикуров</p>
+        </div>
+        <div class="col-xs-6 text-right">
+          <a target="_blank" href="http://vk.com/id139677998"><i class="fa fa-vk" aria-hidden="true"></i></a>
+          <a target="_blank" href="https://www.facebook.com/profile.php?id=100012253260685&pnref"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+          <a target="_blank" href="https://www.youtube.com/user/ThePractik01/"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+          <a target="_blank" href="https://www.instagram.com/yuchikurov/"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- script tags
+  ============================================================= -->
+  <script src="<?php bloginfo('template_url'); ?>/js/jquery-2.1.1.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/gmaps.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/smoothscroll.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/custom.js"></script>
+</body>
+</html>
