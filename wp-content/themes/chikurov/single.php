64dff9abc31d0228ec344fbb2c7d1beea2361189
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-12-05 16:20:00 +0300

    chikurov-32

--- wp-content/themes/chikurov/single.php
+++ wp-content/themes/chikurov/single.php
@@ -1,15 +1,55 @@
 <?php get_header(); ?>
-  <section class="page text-center">
+  <section class="doctor seminar text-center" id="doctor">
     <div class="container">
       <div class="row">
-        <!-- <div class="col-md-12"><h2>Чикуров Юрий Валентинович</h2></div> -->
         <div class="col-md-12">
-          <h4>
-            Доктор Ю. Чикуров, кандидат медицинских наук, доцент, специалист в области неврологии и нейрофизиологии. 
-          </h4>
-          
+          <h2>Базовый семинар по Биологическому центрированию</h2>
+        </div>
+        <div class="col-md-6 col-sm-6 col-xs-12 seminar_article">
+          <div class="seminar_params clearfix">
+            <div class="pull-left">
+              <span class="fa fa-calendar"></span> Дата проведения:
+            </div> 
+            <div class="pull-right">19.01.2018 — 21.01.2018</div>
+          </div>
+          <div class="seminar_params clearfix">
+            <div class="pull-left">
+              <span class="fa fa-map-marker"></span> Место проведения:
+            </div>
+            <span class="pull-right">Москва, Сельскохозяйственная 17, к.5, ауд 128</span>
+          </div>
+          <div class="pull-right btn btn-primary">Записаться</div>
+        </div>
+        <div class="col-md-6 col-sm-6 col-xs-12 seminar_article">
+          <div class="seminar_params clearfix">
+            <div class="pull-left">
+              <span class="fa fa-user"></span> Читает:
+            </div> 
+            <div class="pull-right">Юрий Чикуров</div>
+          </div>
+          <div class="seminar_params clearfix">
+            <div class="pull-left">
+              <span class="fa fa-phone"></span> Записаться по телефону:
+            </div>
+            <span class="pull-right">+7 (495) 135-25-48 (Елена)</span>
+          </div>
+          <div class="seminar_params clearfix">
+            <div class="pull-left">
+              <span class="fa fa-envelope"></span> Написать письмо:
+            </div>
+            <span class="pull-right"><a href="mailto:vitova-lena@mail.ru">vitova-lena@mail.ru</a></span>
+          </div>
+        </div>
+        <div class="col-md-12 seminar_content">
+          <h4 class="content_center">О чем этот семинар</h4>
+          <p>Смысл предлагаемых технологий заключается в освобождении ума от ненужных рефлекторных, машинных реакций на окружающую реальность. Очень хочется проживать жизнь и ощущать ее во всей полноте, а не в виде шаблонных ответов нашего ума!
+          Что дает клиенту проведение процедуры:</p>
+          <p>1. Повышение эффективности работы мозга – лобные доли быстрее и лучше соображают, понимают людей и то, что они от нас реально хотят, принимают решения и делают выводы из ситуаций; лимбическая система эффективнее организует эмоции и контролирует вегетатику, гормоны, а также останавливает аутоиммунные реакции повреждения тканей; базальные узлы уменьшают напряжение жевательных и скелетных мышц, стабилизируют мышцы/фасции/суставы, а также двигательный стереотип в целом; височные доли уменьшают реакции страха и агрессивности, стабилизируют работу кишечника.</p>
         </div>
       </div>
     </div>
+  </section><!-- end of doctor section -->
+  <section class="doctor seminar text-center" id="doctor">
+    <div id="seminar_map" style="height: 300px;"></div>
   </section>
 <?php get_footer(); ?>
