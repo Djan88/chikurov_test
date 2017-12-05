d95e1e51da1856617e355ab8c7bede4aa1ce9281
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-12-05 13:52:49 +0300

    chikurov-22

--- wp-content/themes/chikurov/seminar.php
+++ wp-content/themes/chikurov/seminar.php
@@ -21,25 +21,25 @@ if ($seminar_start_m == 01) {
   $seminar_start_m = ' января ';
 } else if ($seminar_start_m == 02) {
   $seminar_start_m = ' февраля ';
-} } else if ($seminar_start_m == 03) {
+} else if ($seminar_start_m == 03) {
   $seminar_start_m = ' марта ';
-} } else if ($seminar_start_m == 04) {
+} else if ($seminar_start_m == 04) {
   $seminar_start_m = ' апреля ';
-} } else if ($seminar_start_m == 05) {
+} else if ($seminar_start_m == 05) {
   $seminar_start_m = ' мая ';
-} } else if ($seminar_start_m == 06) {
+} else if ($seminar_start_m == 06) {
   $seminar_start_m = ' июня ';
-} } else if ($seminar_start_m == 07) {
+} else if ($seminar_start_m == 07) {
   $seminar_start_m = ' июля ';
-} } else if ($seminar_start_m == 08) {
+} else if ($seminar_start_m == 08) {
   $seminar_start_m = ' августа ';
-} } else if ($seminar_start_m == 09) {
+} else if ($seminar_start_m == 09) {
   $seminar_start_m = ' сентября ';
-} } else if ($seminar_start_m == 10) {
+} else if ($seminar_start_m == 10) {
   $seminar_start_m = ' октября ';
-} } else if ($seminar_start_m == 11) {
+} else if ($seminar_start_m == 11) {
   $seminar_start_m = ' ноября ';
-} } else if ($seminar_start_m == 12) {
+} else if ($seminar_start_m == 12) {
   $seminar_start_m = ' декабря ';
 }
 
@@ -52,25 +52,25 @@ if ($seminar_end_m == 01) {
   $seminar_end_m = ' января ';
 } else if ($seminar_end_m == 02) {
   $seminar_end_m = ' февраля ';
-} } else if ($seminar_end_m == 03) {
+} else if ($seminar_end_m == 03) {
   $seminar_end_m = ' марта ';
-} } else if ($seminar_end_m == 04) {
+} else if ($seminar_end_m == 04) {
   $seminar_end_m = ' апреля ';
-} } else if ($seminar_end_m == 05) {
+} else if ($seminar_end_m == 05) {
   $seminar_end_m = ' мая ';
-} } else if ($seminar_end_m == 06) {
+} else if ($seminar_end_m == 06) {
   $seminar_end_m = ' июня ';
-} } else if ($seminar_end_m == 07) {
+} else if ($seminar_end_m == 07) {
   $seminar_end_m = ' июля ';
-} } else if ($seminar_end_m == 08) {
+} else if ($seminar_end_m == 08) {
   $seminar_end_m = ' августа ';
-} } else if ($seminar_end_m == 09) {
+} else if ($seminar_end_m == 09) {
   $seminar_end_m = ' сентября ';
-} } else if ($seminar_end_m == 10) {
+} else if ($seminar_end_m == 10) {
   $seminar_end_m = ' октября ';
-} } else if ($seminar_end_m == 11) {
+} else if ($seminar_end_m == 11) {
   $seminar_end_m = ' ноября ';
-} } else if ($seminar_end_m == 12) {
+} else if ($seminar_end_m == 12) {
   $seminar_end_m = ' декабря ';
 }
 $seminar_city = get_field('city');
