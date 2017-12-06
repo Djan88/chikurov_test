29e4c63decc8123437d2809b391da3aee4ffd8d9
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-12-06 13:50:36 +0300

    chikurov-33

--- wp-content/themes/chikurov/single.php
+++ wp-content/themes/chikurov/single.php
@@ -1,4 +1,148 @@
 <?php get_header(); ?>
+<?php if (in_category(2)) { ?>
+  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <?php
+    $seminar_start = get_field('seminar_start');
+    $date_formated_start = strtotime($seminar_start);
+    $seminar_start_d = date('d',$date_formated_start);
+    $seminar_start_m = date('m',$date_formated_start);
+    $seminar_start_y = date('Y',$date_formated_start);
+    if ($seminar_start_m == 01) {
+      $seminar_start_m = ' января ';
+    } else if ($seminar_start_m == 02) {
+      $seminar_start_m = ' февраля ';
+    } else if ($seminar_start_m == 03) {
+      $seminar_start_m = ' марта ';
+    } else if ($seminar_start_m == 04) {
+      $seminar_start_m = ' апреля ';
+    } else if ($seminar_start_m == 05) {
+      $seminar_start_m = ' мая ';
+    } else if ($seminar_start_m == 06) {
+      $seminar_start_m = ' июня ';
+    } else if ($seminar_start_m == 07) {
+      $seminar_start_m = ' июля ';
+    } else if ($seminar_start_m == 08) {
+      $seminar_start_m = ' августа ';
+    } else if ($seminar_start_m == 09) {
+      $seminar_start_m = ' сентября ';
+    } else if ($seminar_start_m == 10) {
+      $seminar_start_m = ' октября ';
+    } else if ($seminar_start_m == 11) {
+      $seminar_start_m = ' ноября ';
+    } else if ($seminar_start_m == 12) {
+      $seminar_start_m = ' декабря ';
+    }
+
+    $seminar_end = get_field('seminar_end');
+    $date_formated_end = strtotime($seminar_end);
+    $seminar_end_d = date('d',$date_formated_end);
+    $seminar_end_m = date('m',$date_formated_end);
+    $seminar_end_y = date('Y',$date_formated_end);
+    if ($seminar_end_m == 01) {
+      $seminar_end_m = ' января ';
+    } else if ($seminar_end_m == 02) {
+      $seminar_end_m = ' февраля ';
+    } else if ($seminar_end_m == 03) {
+      $seminar_end_m = ' марта ';
+    } else if ($seminar_end_m == 04) {
+      $seminar_end_m = ' апреля ';
+    } else if ($seminar_end_m == 05) {
+      $seminar_end_m = ' мая ';
+    } else if ($seminar_end_m == 06) {
+      $seminar_end_m = ' июня ';
+    } else if ($seminar_end_m == 07) {
+      $seminar_end_m = ' июля ';
+    } else if ($seminar_end_m == 08) {
+      $seminar_end_m = ' августа ';
+    } else if ($seminar_end_m == 09) {
+      $seminar_end_m = ' сентября ';
+    } else if ($seminar_end_m == 10) {
+      $seminar_end_m = ' октября ';
+    } else if ($seminar_end_m == 11) {
+      $seminar_end_m = ' ноября ';
+    } else if ($seminar_end_m == 12) {
+      $seminar_end_m = ' декабря ';
+    }
+    ?>
+    <section class="doctor seminar text-center" id="doctor">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h2><?php the_title(); ?></h2>
+          </div>
+          <div class="col-md-6 col-sm-6 col-xs-12 seminar_article">
+            <div class="seminar_params clearfix">
+              <div class="pull-left">
+                <span class="fa fa-calendar"></span> Дата проведения:
+              </div> 
+              <div class="pull-right">
+                <?php 
+                  echo $seminar_start_d;
+                  echo ' ';
+                  echo $seminar_start_m;
+                  echo ' ';
+                  echo $seminar_start_y;
+                ?> — 
+                <?php if ($seminar_end) {
+                  echo $seminar_end_d;
+                  echo ' ';
+                  echo $seminar_end_m;
+                  echo ' ';
+                  echo $seminar_end_y;
+                } else {
+                  echo $seminar_start_d;
+                  echo ' ';
+                  echo $seminar_start_m;
+                  echo ' ';
+                  echo $seminar_start_y;
+                }
+                ?>
+              </div>
+            </div>
+            <div class="seminar_params clearfix">
+              <div class="pull-left">
+                <span class="fa fa-map-marker"></span> Место проведения:
+              </div>
+              <span class="pull-right">Москва, Сельскохозяйственная 17, к.5, ауд 128</span>
+            </div>
+            <div class="pull-right btn btn-primary">Записаться</div>
+          </div>
+          <div class="col-md-6 col-sm-6 col-xs-12 seminar_article">
+            <div class="seminar_params clearfix">
+              <div class="pull-left">
+                <span class="fa fa-user"></span> Читает:
+              </div> 
+              <div class="pull-right">Юрий Чикуров</div>
+            </div>
+            <div class="seminar_params clearfix">
+              <div class="pull-left">
+                <span class="fa fa-phone"></span> Записаться по телефону:
+              </div>
+              <span class="pull-right">+7 (495) 135-25-48 (Елена)</span>
+            </div>
+            <div class="seminar_params clearfix">
+              <div class="pull-left">
+                <span class="fa fa-envelope"></span> Написать письмо:
+              </div>
+              <span class="pull-right"><a href="mailto:vitova-lena@mail.ru">vitova-lena@mail.ru</a></span>
+            </div>
+          </div>
+          <div class="col-md-12 seminar_content">
+            <h4 class="content_center">О чем этот семинар</h4>
+            <p>Смысл предлагаемых технологий заключается в освобождении ума от ненужных рефлекторных, машинных реакций на окружающую реальность. Очень хочется проживать жизнь и ощущать ее во всей полноте, а не в виде шаблонных ответов нашего ума!
+            Что дает клиенту проведение процедуры:</p>
+            <p>1. Повышение эффективности работы мозга – лобные доли быстрее и лучше соображают, понимают людей и то, что они от нас реально хотят, принимают решения и делают выводы из ситуаций; лимбическая система эффективнее организует эмоции и контролирует вегетатику, гормоны, а также останавливает аутоиммунные реакции повреждения тканей; базальные узлы уменьшают напряжение жевательных и скелетных мышц, стабилизируют мышцы/фасции/суставы, а также двигательный стереотип в целом; височные доли уменьшают реакции страха и агрессивности, стабилизируют работу кишечника.</p>
+          </div>
+        </div>
+      </div>
+    </section><!-- end of doctor section -->
+    <section class="doctor seminar text-center" id="doctor">
+      <div id="seminar_map" style="height: 300px;"></div>
+    </section>
+  <?php endwhile; else: ?>
+    <?php _e('Sorry, no posts matched your criteria.'); ?>
+  <?php endif; ?>
+<?php } else { ?>
   <section class="doctor seminar text-center" id="doctor">
     <div class="container">
       <div class="row">
@@ -52,4 +196,5 @@
   <section class="doctor seminar text-center" id="doctor">
     <div id="seminar_map" style="height: 300px;"></div>
   </section>
+<?php } ?>
 <?php get_footer(); ?>
