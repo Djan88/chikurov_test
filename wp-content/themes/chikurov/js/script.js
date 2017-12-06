8d150e6a451f23c44b3ba899ca087bbc060735cf
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-12-06 19:03:41 +0300

    chikurov-75

--- wp-content/themes/chikurov/js/script.js
+++ wp-content/themes/chikurov/js/script.js
@@ -1,4 +1,6 @@
 jQuery( function() {
   jQuery('.seminar_title').val(jQuery('.page_title').text())
   jQuery('.seminar_url').val(jQuery('.page_url').text())
+  console.log(jQuery('.page_title').text())
+  console.log(jQuery('.page_url').text())
 });
