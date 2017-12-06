6e6cc060ea1e97a9f8102c8276a7e33138986852
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-12-06 13:54:03 +0300

    chikurov-34

--- wp-content/themes/chikurov/single.php
+++ wp-content/themes/chikurov/single.php
@@ -103,7 +103,7 @@
               <div class="pull-left">
                 <span class="fa fa-map-marker"></span> Место проведения:
               </div>
-              <span class="pull-right">Москва, Сельскохозяйственная 17, к.5, ауд 128</span>
+              <span class="pull-right"><?php the_field('adres'); ?></span>
             </div>
             <div class="pull-right btn btn-primary">Записаться</div>
           </div>
