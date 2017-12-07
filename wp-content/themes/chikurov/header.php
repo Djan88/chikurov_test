7bab9d818cf71c6014c78e79e85606549f639893
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-12-07 11:26:43 +0300

    chikurov-89

--- wp-content/themes/chikurov/header.php
+++ wp-content/themes/chikurov/header.php
@@ -33,7 +33,7 @@
           <a target="_blank" href="https://www.youtube.com/user/ThePractik01/" class="media_area_item media_area_item-youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a>
           <a target="_blank" href="https://www.instagram.com/yuchikurov/" class="media_area_item media_area_item-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
         </div>
-        <div class="col-md-8 col-xs-12 main-menu">
+        <div class="col-md-9 col-xs-12 main-menu">
           <div class="row">
             <!--<div class="col-md-12 contact_area">
               <div class="row">
@@ -68,7 +68,7 @@
             </div>     
           </div>
         </div>
-        <div class="col-xs-2 col-sm-2 col-md-2 header-logo center-block">
+        <div class="col-xs-2 col-sm-2 col-md-1 header-logo center-block">
           <br>
           <a href="/"><img src="<?php bloginfo('template_url'); ?>/img/bc_2.png" alt="" class="img-responsive logo"></a>
         </div>
