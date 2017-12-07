82915052667434e2a01afcdbe0ccb17945ed471b
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-12-07 12:20:58 +0300

    chikurov_header-3

--- wp-content/themes/chikurov/header.php
+++ wp-content/themes/chikurov/header.php
@@ -23,20 +23,24 @@
   <header class="top-header">
     <div class="container">
       <div class="row">
-        <div class="col-md-6 col-md-offset-3 contacts">
-          <a class="contacts_email" href="mailto:info@chikurov.com">info@chikurov.com</a>
-          <a class="contacts_home" href="/"> Школа Доктора Чикурова </a>
-          <span class="contacts_phone">+7 (495) 135-25-48</span>
-        </div>
-      </div>
-      <div class="row">
-        <div class="media_area col-md-2 col-xs-4">
+        <div class="media_area col-md-1 col-xs-4">
           <a target="_blank" href="http://vk.com/id139677998" class="media_area_item media_area_item-vk"><i class="fa fa-vk" aria-hidden="true"></i></a>
           <a target="_blank" href="https://www.facebook.com/profile.php?id=100012253260685&pnref" class="media_area_item media_area_item-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
           <a target="_blank" href="https://www.youtube.com/user/ThePractik01/" class="media_area_item media_area_item-youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a>
           <a target="_blank" href="https://www.instagram.com/yuchikurov/" class="media_area_item media_area_item-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
         </div>
-        <div class="col-md-9 col-xs-12 main-menu">
+        <div class="col-md-6 col-md-offset-1 contacts">
+          <a class="contacts_email" href="mailto:info@chikurov.com">info@chikurov.com</a>
+          <a class="contacts_home" href="/"> Школа Доктора Чикурова </a>
+          <span class="contacts_phone">+7 (495) 135-25-48</span>
+        </div>
+        <div class="col-xs-2 col-sm-2 col-md-1 header-logo center-block">
+          <br>
+          <a href="/"><img src="<?php bloginfo('template_url'); ?>/img/bc_2.png" alt="" class="img-responsive logo"></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2 col-xs-12 main-menu">
           <div class="row">
             <div class="col-md-12">
               <nav class="navbar navbar-default">
@@ -65,10 +69,6 @@
               </nav>
             </div>     
           </div>
-        </div>
-        <div class="col-xs-2 col-sm-2 col-md-1 header-logo center-block">
-          <br>
-          <a href="/"><img src="<?php bloginfo('template_url'); ?>/img/bc_2.png" alt="" class="img-responsive logo"></a>
         </div>
       </div>
     </div>
