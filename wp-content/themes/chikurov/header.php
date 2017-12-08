57ab50a7fe3337a9fca654e8cfb13f672a8c2fbd
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-12-08 14:39:17 +0300

    header-3

--- wp-content/themes/chikurov/header.php
+++ wp-content/themes/chikurov/header.php
@@ -47,7 +47,7 @@
                 <div class="container-fluid nav-bar">
                   <!-- Brand and toggle get grouped for better mobile display -->
                   <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed hidden" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                       <span class="sr-only">Toggle navigation</span>
                       <span class="icon-bar"></span>
                       <span class="icon-bar"></span>
@@ -56,8 +56,8 @@
                   </div>
 
                   <!-- Collect the nav links, forms, and other content for toggling -->
-                  <!-- <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"> -->
-                  <div id="bs-example-navbar-collapse-1">
+                  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <!-- <div id="bs-example-navbar-collapse-1"> -->
                     
                     <ul class="nav navbar-nav navbar-left">
                       <li><a class="menu" href="/#seminars">Семинары</a></li>
