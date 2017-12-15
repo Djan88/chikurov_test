310c652b32e3d648734fde4af8aa9d7ef309d734
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-12-15 15:24:37 +0300

    ststus

--- wp-content/themes/chikurov/front-page.php
+++ wp-content/themes/chikurov/front-page.php
@@ -41,12 +41,12 @@
             endwhile;
             wp_reset_postdata();
             ?>
-            <?php if (  $querySeminar->max_num_pages > 1 ) : ?>
+            <?php if (  $wp_query->max_num_pages > 1 ) : ?>
                 <script>
                     var ajaxurl = '<?php echo site_url() ?>/wp-admin/admin-ajax.php';
-                    var true_posts = '<?php echo serialize($querySeminar->query_vars); ?>';
+                    var true_posts = '<?php echo serialize($wp_query->query_vars); ?>';
                     var current_page = <?php echo (get_query_var('paged')) ? get_query_var('paged') : 1; ?>;
-                    var max_pages = '<?php echo $querySeminar->max_num_pages; ?>';
+                    var max_pages = '<?php echo $wp_query->max_num_pages; ?>';
                 </script>
                 <div href="seminar.html" id="true_loadmore" class="btn btn-primary btn-lg">Загрузить еще</div>
             <?php endif; ?>
