58ce8d48d281ed3f35b749462aa38c9689a34df9
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-12-15 17:36:36 +0300

    sems-36

--- wp-content/themes/chikurov/footer.php
+++ wp-content/themes/chikurov/footer.php
@@ -38,6 +38,33 @@
           </div>
       </div>
   </div>
+  <script>
+    jQuery(function($){
+        $('#loadSeminar').on('click', function(event) {
+            $(this).text('Загружаю...'); // изменяем текст кнопки, вы также можете добавить прелоадер
+            console.log('test');
+            var data = {
+                'action': 'loadmore',
+                'query': true_posts,
+                'page' : current_page
+            };
+            $.ajax({
+                url:ajaxurl, // обработчик
+                data:data, // данные
+                type:'POST', // тип запроса
+                success:function(data){
+                    if( data ) {
+                        $('#loadSeminar').text('Загрузить ещё').before(data); // вставляем новые посты
+                        current_page++; // увеличиваем номер страницы на единицу
+                        if (current_page == max_pages) $("#loadSeminar").remove(); // если последняя страница, удаляем кнопку
+                    } else {
+                        $('#loadSeminar').remove(); // если мы дошли до последней страницы постов, скроем кнопку
+                    }
+                }
+            });
+        });
+    });
+  </script>
   <!-- Yandex.Metrika counter -->
   <script type="text/javascript" >
       (function (d, w, c) {
