34eb24f61418b96ca551c980ec712c8f59973adb
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-12-15 16:34:47 +0300

    sems-23

--- wp-content/themes/chikurov/front-page.php
+++ wp-content/themes/chikurov/front-page.php
@@ -38,15 +38,16 @@
             while ($wp_query->have_posts()) : $wp_query->the_post();
                 get_template_part( 'seminar');
             endwhile;
+            wp_reset_postdata();
             ?>
             <?php if (  $wp_query->max_num_pages > 1 ) : ?>
-                <script>
-                    var ajaxurl = '<?php echo site_url() ?>/wp-admin/admin-ajax.php';
-                    var true_posts = '<?php echo serialize($wp_query->query_vars); ?>';
-                    var current_page = <?php echo (get_query_var('paged')) ? get_query_var('paged') : 1; ?>;
-                    var max_pages = '<?php echo $wp_query->max_num_pages; ?>';
-                </script>
-                <div id="true_loadmore" class="btn btn-primary btn-lg">Загрузить еще</div>
+              <script>
+                var ajaxurl = '<?php echo site_url() ?>/wp-admin/admin-ajax.php';
+                var true_posts = '<?php echo serialize($wp_query->query_vars); ?>';
+                var current_page = <?php echo (get_query_var('paged')) ? get_query_var('paged') : 1; ?>;
+                var max_pages = '<?php echo $wp_query->max_num_pages; ?>';
+              </script>
+              <div id="true_loadmore">Загрузить ещё</div>
             <?php endif; ?>
           </div>
         </div>
