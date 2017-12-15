6e4ed81fa37c950776d3b35d734bbac62fef22f5
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-12-15 16:45:47 +0300

    sems-25

--- wp-content/themes/chikurov/front-page.php
+++ wp-content/themes/chikurov/front-page.php
@@ -1,74 +1,77 @@
-<?php get_header(); ?>
-  <section class="doctor text-center" id="doctor">
-    <div class="container">
-      <div class="row">
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-          <?php
-          the_content(__('(more...)'));
-          edit_post_link(__('Edit This'));
-          ?>
-        <?php endwhile; else: ?>
-          <?php _e('Sorry, no posts matched your criteria.'); ?>
-        <?php endif; ?>
-      </div>
-    </div>
-  </section><!-- end of doctor section -->
-  <section class="seminars text-center" id="seminars">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12"><h2>Расписание семинаров</h2></div>
-        <div class="col-md-12 seminar_wrap">
-          <div class="row">
-            <?php
-            // The Query
-            $wp_query = new WP_Query( array(
-                'category_name' => 'seminary',
-                'order' => 'ASC',
-                'orderby' => 'meta_value',
-                'meta_key' => 'seminar_start',
-                'meta_query' => array(
-                    array(
-                        'key' => 'seminar_start',
-                        'value' => date('Y-m-d'),
-                        'compare' => '>=',
-                        'type' => 'DATE'
-                    )
-                )
-            ));
-            while ($wp_query->have_posts()) : $wp_query->the_post();
-                get_template_part( 'seminar');
-            endwhile;
-            wp_reset_postdata();
-            ?>
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+<div class="wrap">
+  <?php if ( is_home() && ! is_front_page() ) : ?>
+    <header class="page-header">
+      <h1 class="page-title"><?php single_post_title(); ?></h1>
+    </header>
+  <?php else : ?>
+  <header class="page-header">
+    <h2 class="page-title"><?php _e( 'Posts', 'twentyseventeen' ); ?></h2>
+  </header>
+  <?php endif; ?>
+
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+
+      <?php
+      if ( have_posts() ) :
+
+        /* Start the Loop */
+        while ( have_posts() ) : the_post();
+
+          /*
+           * Include the Post-Format-specific template for the content.
+           * If you want to override this in a child theme, then include a file
+           * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+           */
+          get_template_part( 'template-parts/post/content', get_post_format() );
+
+        endwhile;
+
+        the_posts_pagination( array(
+          'prev_text' => '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
+          'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>',
+          'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
+        ) );
+
+
+      else :
+
+        get_template_part( 'template-parts/post/content', 'none' );
+
+      endif;
+      ?>
             <?php if (  $wp_query->max_num_pages > 1 ) : ?>
-              <script>
-                var ajaxurl = '<?php echo site_url() ?>/wp-admin/admin-ajax.php';
-                var true_posts = '<?php echo serialize($wp_query->query_vars); ?>';
-                var current_page = <?php echo (get_query_var('paged')) ? get_query_var('paged') : 1; ?>;
-                var max_pages = '<?php echo $wp_query->max_num_pages; ?>';
-              </script>
-              <div id="true_loadmore" class="btn btn-primary btn-lg">Загрузить ещё</div>
+                <script>
+                    var ajaxurl = '<?php echo site_url() ?>/wp-admin/admin-ajax.php';
+                    var true_posts = '<?php echo serialize($wp_query->query_vars); ?>';
+                    var current_page = <?php echo (get_query_var('paged')) ? get_query_var('paged') : 1; ?>;
+                    var max_pages = '<?php echo $wp_query->max_num_pages; ?>';
+                </script>
+                <div id="true_loadmore">Загрузить ещё</div>
             <?php endif; ?>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <!-- end of doctor section -->
-  <section class="doctor seminar text-center" id="bc">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <h2>Биологическое центрирование</h2>
-        </div>
-        <div class="col-md-12 bc">
-          <img class="img-responsive pull-left content_img colorbox-147" src="http://yuchikurov.info/wp-content/themes/chikurov/img/oblojka.png" alt="">
-          <p>Биологическое Центрирование объединяет людей с совершенно различным образованием и профессиями. Эти люди хотят познать принципы формирования здорового и успешного жизнеустройства и проистекающих из него физического здоровья, личностного развития, успешной самореализации в жизни. </p>
-          <p>Биологическое Центрирование объясняет не только причины болезней и жизненных неудач, но и место Человека в общем мироустройстве.</p>
-          <p>Биологическое Центрирование является продуктом более чем четверть векового моего практического опыта работы с клиентами, предельно холистично по своей сути – и объединяет в себе анатомию/физиологию/клинику с невидимыми глазу и приборам, тонкоматериальными процессами Ума, которые помимо физического тела составляют истинную суть человека.  Эти тонкоматериальные человеческие процессы на стыке души/ума/тела ранее всегда были прерогативой различных практик и религий, а в Биологическом Центрировании, пожалуй, впервые, сведены во вполне материальные и реально работающие алгоритмы схемы и протоколы действия.</p>
-          <a target="_blank" href="http://www.yuchikurov.info/biologicheskoe-centrirovanie/" class="btn btn-primary btn-lg">Читать книгу</a>
-        </div>
-      </div>
-    </div>
-  </section>
-<?php get_footer(); ?>
+
+    </main><!-- #main -->
+  </div><!-- #primary -->
+  <?php get_sidebar(); ?>
+</div><!-- .wrap -->
+
+<?php get_footer();
