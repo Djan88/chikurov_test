70cc9b6f0b0a1458ce19c61df8940d7abab7a4c3
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-03-10 21:42:55 +0300

    seminar months-7

--- wp-content/themes/chikurov/seminar.php
+++ wp-content/themes/chikurov/seminar.php
@@ -94,19 +94,19 @@ global $cur_year;
 global $cur_month;
 $seminar_city = get_field('city');
 $seminar_autor = get_field('autor');
-// echo '<div class="seminar_item col-md-12"';
+echo '<div class="seminar_item col-md-12"';
 if (!$cur_month && global $cur_month < $seminar_start_m) {
-  // echo '<h3 class="seminar_heading">';
+  echo '<h3 class="seminar_heading">';
   global $cur_month = $month_names[$seminar_start_m];
-  // echo $cur_month;
+  echo $cur_month;
   if (!$cur_year && global $cur_year < $seminar_start_y) {
     global $cur_year = $seminar_start_y;
   }
-  // echo ' ';
-  // echo $cur_year;
-  // echo '</h3>';
+  echo ' ';
+  echo $cur_year;
+  echo '</h3>';
 }
-// echo "</div>";
+echo "</div>";
 echo '<div class="seminar_item col-md-12"';
 echo 'data-start="';
 echo $seminar_start;
