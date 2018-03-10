2ba443ad8fd0636b1e2b111d3e1908b8b8055eaf
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-03-10 23:44:25 +0300

    seminar months-42

--- wp-content/themes/chikurov/seminar.php
+++ wp-content/themes/chikurov/seminar.php
@@ -99,7 +99,7 @@ if (!$cur_month && $cur_month != $seminar_start_months) {
   echo '<div class="seminar_item col-md-12 seminar_heading_wrap">';
   echo '<h3 class="seminar_heading">';
   $cur_month = $month_names[$seminar_start_months];
-  echo $seminar_start_months;
+  echo $cur_month;
   global $cur_year;
   if (!$cur_year && $cur_year < $seminar_start_y) {
     $cur_year = $seminar_start_y;
