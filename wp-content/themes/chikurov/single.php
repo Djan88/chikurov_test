8f70d9b18d29a299832be52507124c1e95c6f0b1
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-06-18 12:33:47 +0800

    seminar-3

--- wp-content/themes/chikurov/single.php
+++ wp-content/themes/chikurov/single.php
@@ -176,8 +176,10 @@
               <?php } else { ?>
                 <?php if ($autor == 'Юрий Чикуров') {?>
                   <?php echo do_shortcode('[contact-form-7 id="4" title="Запись на семинар"]'); ?>
-                <?php } else { ?>
+                <?php } else if ($autor == 'Ирина Иванова') { ?>
                   <?php echo do_shortcode('[contact-form-7 id="170" title="Запись на семинар_Иванова"]'); ?>
+                <?php } else if ($autor == 'Василий Киселев') { ?>
+                  <?php echo do_shortcode('[contact-form-7 id="170" title="Запись на семинар_Киселев"]'); ?>
                 <?php } ?>
               <?php } ?>
             </div>
