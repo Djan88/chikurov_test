d24dcdf7bf17ac454d0c26bdc28ef11232e343fc
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-06-23 09:15:56 +0300

    seminar-13

--- wp-content/themes/chikurov/front-page.php
+++ wp-content/themes/chikurov/front-page.php
@@ -17,14 +17,12 @@
     <div class="container">
       <div class="row">
         <div class="col-md-12"><h2>Расписание семинаров</h2></div>
-        <div class="col-md-6 col-sm-10 col-xs-12">
-          <div class="btn-group">
-            <button type="button" data-filter="all" class="btn btn-warning btn_city_filter active">Все</button>
-            <button type="button" data-filter="msk" class="btn btn-warning btn_city_filter">Москва</button>
-            <button type="button" data-filter="spb" class="btn btn-warning btn_city_filter">Санкт—Петербург</button>
-            <button type="button" data-filter="kiev" class="btn btn-warning btn_city_filter">Киев</button>
-            <button type="button" data-filter="ast" class="btn btn-warning btn_city_filter">Астана</button>
-          </div>
+        <div class="col-md-6 col-md-offset-3 col-sm-10 col-sm-offset-1 col-xs-12">
+          <button type="button" data-filter="all" class="btn btn-warning btn_city_filter active">Все</button>
+          <button type="button" data-filter="msk" class="btn btn-warning btn_city_filter">Москва</button>
+          <button type="button" data-filter="spb" class="btn btn-warning btn_city_filter">Санкт—Петербург</button>
+          <button type="button" data-filter="kiev" class="btn btn-warning btn_city_filter">Киев</button>
+          <button type="button" data-filter="ast" class="btn btn-warning btn_city_filter">Астана</button>
         </div>
         <div class="col-md-12 seminar_wrap">
           <div class="row">
