ecd366566a5ffd0b4ef127777427baa50c6fa8a5
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-08-01 13:55:25 +0300

    single-3

--- wp-content/themes/chikurov/single.php
+++ wp-content/themes/chikurov/single.php
@@ -5,62 +5,62 @@
     $seminar_start = get_field('seminar_start');
     $date_formated_start = strtotime($seminar_start);
     $seminar_start_d = date('d',$date_formated_start);
-    $seminar_start_m = date('m',$date_formated_start);
+    $seminar_start_m = strval(date('m',$date_formated_start));
     $seminar_start_y = date('Y',$date_formated_start);
-    if ($seminar_start_m == 01) {
+    if ($seminar_start_m == '01') {
       $seminar_start_m = ' января ';
-    } else if ($seminar_start_m == 02) {
+    } else if ($seminar_start_m == '02') {
       $seminar_start_m = ' февраля ';
-    } else if ($seminar_start_m == 03) {
+    } else if ($seminar_start_m == '03') {
       $seminar_start_m = ' марта ';
-    } else if ($seminar_start_m == 04) {
+    } else if ($seminar_start_m == '04') {
       $seminar_start_m = ' апреля ';
-    } else if ($seminar_start_m == 05) {
+    } else if ($seminar_start_m == '05') {
       $seminar_start_m = ' мая ';
-    } else if ($seminar_start_m == 06) {
+    } else if ($seminar_start_m == '06') {
       $seminar_start_m = ' июня ';
-    } else if ($seminar_start_m == 07) {
+    } else if ($seminar_start_m == '07') {
       $seminar_start_m = ' июля ';
-    } else if ($seminar_start_m == 08) {
+    } else if ($seminar_start_m == '08') {
       $seminar_start_m = ' августа ';
-    } else if ($seminar_start_m == 09) {
+    } else if ($seminar_start_m == '09') {
       $seminar_start_m = ' сентября ';
-    } else if ($seminar_start_m == 10) {
+    } else if ($seminar_start_m == '10') {
       $seminar_start_m = ' октября ';
-    } else if ($seminar_start_m == 11) {
+    } else if ($seminar_start_m == '11') {
       $seminar_start_m = ' ноября ';
-    } else if ($seminar_start_m == 12) {
+    } else if ($seminar_start_m == '12') {
       $seminar_start_m = ' декабря ';
     }
 
     $seminar_end = get_field('seminar_end');
     $date_formated_end = strtotime($seminar_end);
     $seminar_end_d = date('d',$date_formated_end);
-    $seminar_end_m = date('m',$date_formated_end);
+    $seminar_end_m = strval(date('m',$date_formated_end));
     $seminar_end_y = date('Y',$date_formated_end);
-    if ($seminar_end_m == 01) {
+    if ($seminar_end_m == '01') {
       $seminar_end_m = ' января ';
-    } else if ($seminar_end_m == 02) {
+    } else if ($seminar_end_m == '02') {
       $seminar_end_m = ' февраля ';
-    } else if ($seminar_end_m == 03) {
+    } else if ($seminar_end_m == '03') {
       $seminar_end_m = ' марта ';
-    } else if ($seminar_end_m == 04) {
+    } else if ($seminar_end_m == '04') {
       $seminar_end_m = ' апреля ';
-    } else if ($seminar_end_m == 05) {
+    } else if ($seminar_end_m == '05') {
       $seminar_end_m = ' мая ';
-    } else if ($seminar_end_m == 06) {
+    } else if ($seminar_end_m == '06') {
       $seminar_end_m = ' июня ';
-    } else if ($seminar_end_m == 07) {
+    } else if ($seminar_end_m == '07') {
       $seminar_end_m = ' июля ';
-    } else if ($seminar_end_m == 08) {
+    } else if ($seminar_end_m == '08') {
       $seminar_end_m = ' августа ';
-    } else if ($seminar_end_m == 09) {
+    } else if ($seminar_end_m == '09') {
       $seminar_end_m = ' сентября ';
-    } else if ($seminar_end_m == 10) {
+    } else if ($seminar_end_m == '10') {
       $seminar_end_m = ' октября ';
-    } else if ($seminar_end_m == 11) {
+    } else if ($seminar_end_m == '11') {
       $seminar_end_m = ' ноября ';
-    } else if ($seminar_end_m == 12) {
+    } else if ($seminar_end_m == '12') {
       $seminar_end_m = ' декабря ';
     }
     ?>
