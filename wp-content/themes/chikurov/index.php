da7abbdc8511468187db8937c4ecbf2cd7c7c0e5
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-08-08 17:23:56 +0300

    reviews

--- wp-content/themes/chikurov/index.php
+++ wp-content/themes/chikurov/index.php
@@ -1,5 +1,47 @@
 <?php get_header(); ?>
-<?php if (is_page(546) || is_page(630)) { ?>
+<?php if (is_categoy(9)) { ?>
+  <section class="seminars text-center reviews_wrap" id="lectors">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <?php
+          // The Query
+          $query_reviews = new WP_Query( array( 'category_name' => 'otzyvy', 'posts_per_page' => '4' ) );
+          $cur_month = 0;
+          while ($query_reviews->have_posts()) : $query_reviews->the_post();
+              echo '<div class="review col-md-3 col-xs-12">';
+              echo '<a href="';
+              if (get_field('link_review')) {
+                echo the_field('link_review');
+              } else {
+                echo the_permalink();
+              }
+              echo '" target="_blank" title="Ссылка на источник">';
+              echo '<img src="';
+              echo the_field('img_review');
+              echo '" alt="">';
+              echo '</a>';
+              echo '<p>';
+              echo the_content(__('(читать полностью)'));
+              echo '</p>';
+              echo '<a href="';
+              if (get_field('link_review')) {
+                echo the_field('link_review');
+              } else {
+                echo the_permalink();
+              }
+              echo '" target="_blank" title="Ссылка на источник">';
+              echo the_field('name_review');
+              echo '</a>';
+              echo '</div>';
+          endwhile;
+          wp_reset_postdata();
+          ?>
+        </div>
+      </div>
+    </div>
+  </section>
+<?php } else if (is_page(546) || is_page(630)) { ?>
     <section class="seminars text-center" id="seminars">
     <div class="container">
       <div class="row">
