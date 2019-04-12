6216fdbcb53e6512d7d9ab906118b95695ef0add
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-04-12 10:28:06 +0300

    salehova-2

--- wp-content/themes/chikurov/single.php
+++ wp-content/themes/chikurov/single.php
@@ -185,6 +185,8 @@
                   <?php echo do_shortcode('[contact-form-7 id="632" title="Запись на семинар_Киселев"]'); ?>
                 <?php } else if ($autor == 'Елена Зукуле') { ?>
                   <?php echo do_shortcode('[contact-form-7 id="1059" title="Запись на семинар_Зукуле"]'); ?>
+                <?php } else if ($autor == 'Марина Салехова') { ?>
+                  <?php echo do_shortcode('[contact-form-7 id="2711" title="Запись на семинар_Салехова"]'); ?>
                 <?php } ?>
               <?php } ?>
             </div>
