3a7c2846f5ddabd247134858985be837d6143130
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-07-21 13:08:03 +0300

    animate scroll-1

--- wp-content/themes/chikurov/header.php
+++ wp-content/themes/chikurov/header.php
@@ -79,7 +79,7 @@
                   <!-- <div id="bs-example-navbar-collapse-1"> -->
                     
                     <ul class="nav navbar-nav navbar-left">
-                      <li><a class="menu" href="/#seminars">Семинары</a></li>
+                      <li><a class="menu" href="#seminars">Семинары</a></li>
                       <!-- <li><a class="menu" href="#about">Биологическое центрирование</a></li> -->
                       <li><a class="menu" href="http://school-bc.ru/" target="_blank">Онлайн школа</a></li>
                       <li><a class="menu" target="_blank" href="http://www.yuchikurov.info/reestr/">Реестр специалистов</a></li>
