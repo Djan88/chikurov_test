1bbf9e937b85710cf40e32c06990286042b4d1ae
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-08-22 16:05:38 +0300

    pp-1

--- wp-content/themes/chikurov/footer.php
+++ wp-content/themes/chikurov/footer.php
@@ -66,7 +66,7 @@
           <div class="modal-content">
               <div class="modal-header">
                   <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                  <h4 class="modal-title" id="videoCoursLabel">Скачать книгу на Английском</h4>
+                  <h4 class="modal-title" id="videoCoursLabel">Download book for free</h4>
               </div>
               <div class="modal-body">
                   <h4>"Practical Psychosomatics (Практическая Писхосоматика)"</h4>
