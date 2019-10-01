4bf341288bf37c13bb54d6061cfea9dcbe66a250
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-10-01 13:06:06 +0300

    front-1

--- wp-content/themes/chikurov/footer.php
+++ wp-content/themes/chikurov/footer.php
@@ -216,7 +216,7 @@
           <div class="modal-content styled_form_alt">
               <div class="modal-header">
                   <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                  <h4 class="modal-title" id="videoCoursLabel">Терапевтическая сессия</h4>
+                  <h4 class="modal-title" id="videoCoursLabel">Персональная сессия</h4>
               </div>
               <div class="modal-body">
                   <?php echo do_shortcode('[contact-form-7 id="4279" title="Терапевтическая сессия"]')?>
