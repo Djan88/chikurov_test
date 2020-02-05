c36c32d48c2c1264934a76790eb69efbf157a1ad
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-02-05 15:41:16 +0300

    chta_bot-42

--- wp-content/themes/chikurov/js/script.js
+++ wp-content/themes/chikurov/js/script.js
@@ -17,10 +17,12 @@ jQuery(document).ready(function () {
   });
   jQuery('body').on('click', function(event) {
     console.log(event.target);
-    // if (!jQuery('.peekobot_wrap').hasClass('peekobot_wrap-closed')) {
-    //   jQuery('.peekobot_wrap').addClass('peekobot_wrap-closed');
-    //   jQuery('.pekobot_open').removeClass('hidden');
-    // }
+    if (event.target.attr('data-next') == '100') {
+      if (!jQuery('.peekobot_wrap').hasClass('peekobot_wrap-closed')) {
+        jQuery('.peekobot_wrap').addClass('peekobot_wrap-closed');
+        jQuery('.pekobot_open').removeClass('hidden');
+      }
+    }
   });
   jQuery('.pekobot_open').on('click', function(event) {
     if (jQuery('.peekobot_wrap').hasClass('peekobot_wrap-closed')) {
