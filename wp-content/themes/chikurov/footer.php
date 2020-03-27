b99efcf406f1ac46d6f76e31a43e0d841e084c6e
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-03-27 15:54:18 +0300

    scroll-40\

--- wp-content/themes/chikurov/footer.php
+++ wp-content/themes/chikurov/footer.php
@@ -224,7 +224,7 @@
                   <?php //echo do_shortcode('[contact-form-7 id="449" title="Купить ловушку"]')?>
                   <div class="clearfix">
                     <div class="col-md-4" style="text-align: center;">
-                    <img style="max-width: 100%;" class="img-responsive pull-left content_img colorbox-147" src="httpsrov.com/wp-content/uploads/2018/03/IMG_3817-167x300.jpg" alt="">
+                    <img style="max-width: 100%;" class="img-responsive pull-left content_img colorbox-147" src="/wp-content/uploads/2018/03/IMG_3817-167x300.jpg" alt="">
                     </div>
                     <div class="col-md-8">
                       <script id="b85b70eafb20cae19a3f8910bfeaf0f331d89c4d" src="https://school-bc.ru/pl/lite/widget/script?id=24443&rand=kajr"></script>
