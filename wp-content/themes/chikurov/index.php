9523a109b7f2460506e251cc640d6deb4f3a7382
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-04-07 17:04:34 +0300

    structure-125

--- wp-content/themes/chikurov/index.php
+++ wp-content/themes/chikurov/index.php
@@ -89,14 +89,16 @@
                   'meta_key' => 'seminar_start',
                   'meta_query' => array(
                       array(
-                          'key' => 'autor',
-                          'value' => 'Юрий Чикуров'
-                      ),
-                      array(
                           'key' => 'seminar_end',
                           'value' => date('Y-m-d'),
                           'compare' => '>=',
                           'type' => 'DATE'
+                      ),
+                      array(
+                          'key' => 'autor',
+                          'value' => array('Юрий Чикуров'),
+                          'compare' => '=',
+                          'type' => 'CHAR'
                       )
                   )
               ));
