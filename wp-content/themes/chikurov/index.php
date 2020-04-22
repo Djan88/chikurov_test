197b94710a95d180b9b7e04c3f584852ebe8e0dc
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-04-22 17:20:32 +0300

    single-11

--- wp-content/themes/chikurov/index.php
+++ wp-content/themes/chikurov/index.php
@@ -146,7 +146,7 @@
       <div class="col-md-12">
         <h2><?php the_title(); ?></h2>
       </div>
-      <div class="col-md-12">
+      <div class="col-md-12 seminar_content">
         <?php
         the_content(__('(more...)'));
         edit_post_link(__('Edit This'));
