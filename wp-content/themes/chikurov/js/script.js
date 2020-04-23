1766beff298cc076492dee2ca11267397fbcb3a4
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-04-23 15:18:01 +0300

    masters-6

--- wp-content/themes/chikurov/js/script.js
+++ wp-content/themes/chikurov/js/script.js
@@ -59,9 +59,29 @@ jQuery(document).ready(function () {
 
 
   jQuery('.chi_masters').slick({
-    infinite: true,
+    centerMode: true,
+    centerPadding: '60px',
     slidesToShow: 3,
-    slidesToScroll: 3
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 
   jQuery('.btn_seminar_filter').on('click', function(event) {
