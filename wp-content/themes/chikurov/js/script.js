74a923200afff82859f11aa7fcddc83584260963
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-04-23 15:44:22 +0300

    masters-19

--- wp-content/themes/chikurov/js/script.js
+++ wp-content/themes/chikurov/js/script.js
@@ -77,7 +77,7 @@ jQuery(document).ready(function () {
         settings: {
           centerMode: true,
           centerPadding: '40px',
-          slidesToShow: 3
+          slidesToShow: 1
         }
       }
     ]
